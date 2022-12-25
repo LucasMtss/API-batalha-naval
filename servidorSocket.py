db8cbb52dbbcebb87c532313d9bcbf20b28eb230
Lucas Matos <54159333+LucasMtss@users.noreply.github.com>
2022-12-25 20:57:11 -0300

    feat: send messages to players of specified room

--- servidorSocket.py
+++ servidorSocket.py
@@ -1,13 +1,15 @@
-from flask import Flask 
-from flask_socketio import SocketIO, send, emit
+import time
+
 import requests
-import json
+from flask import Flask
+from flask_socketio import SocketIO, emit, join_room, leave_room, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-sala = []
+sala = dict()
+
 
 def createUser(name):
     response = requests.post('http://127.0.0.1:5001/users', {"nome": name})
@@ -16,15 +18,49 @@ def createUser(name):
 
 @socketio.on('message')
 def handleMessage(msg):
-	print('Message: ' + msg)
-	send(msg, broadcast=True)
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
 
-@socketio.on('entrar_sala')
-def handleMessage(playerName):
-    print('sala criada para '+playerName)
+# Verifica se a sala está cheia
+def verify_room():
+    for i in sala:
+        if sala[i]["size"] == 1:
+            return i
 
-    createUserResponse = createUser(playerName)
-    emit('entrar_sala', {'mensagem':'você está coectado na sala '+playerName, 'response': createUserResponse})
+
+# Salva o board do usuario
+@socketio.on("board")
+def send_board(data):
+    room = data["room"]
+    user = data["user"]
+    board = data["board"]
+    sala[room][user] = board
+    emit("send_board", sala[room], room=room)
+
+
+def joinRoom(room, user, num=1):
+    join_room(room)
+    sala[room]["size"] = num
+    sala[room][f'{user}'] = []
+    print(sala[room])
+    emit("room_message", f"Your room: {room}", room=room)
+    if (num == 2):
+        time.sleep(2.5)  # deixar esse sleep por causa do front
+        emit("room_message", "Sala cheia", room=room)
+
+# Inicia ou adiciona um novo usuario na sala
+@socketio.on("join")
+def on_join(data):
+    room = verify_room()
+    user = data["user"]
+    if room == None:
+        room = data["room"]
+        sala[room] = {"jogador1": user}
+        joinRoom(room, user)
+    else:
+        sala[room]["jogador2"] = user
+        joinRoom(room, user, 2)
+
 
 if __name__ == '__main__':
-	socketio.run(app)
+    socketio.run(app, debug=True)
