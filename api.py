d3d181b67cb3e924f07af0f0ad43227a5823ff89
Lucas Matos <54159333+LucasMtss@users.noreply.github.com>
2023-01-13 18:12:49 -0300

    feat: upload files

--- api.py
+++ api.py
@@ -1,19 +1,190 @@
-from flask import Flask
+from textwrap import indent
+from flask import Flask, jsonify, request, Response
+from flask_pymongo import PyMongo
+from bson import json_util
+from bson.objectid import ObjectId
+import json
 from functions import createBattleshipGame
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/batalha_naval'
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def root():
-  return "<h1>API com Flask</h1>"
+    return "<h1>API com Flask</h1>"
+
 
 @app.route('/gerar-campo')
 def generateBattlefield():
-  return {
-    "status": 200,
-    "payload": {
-      "matriz": createBattleshipGame()
-    }
+    return {
+        "status": 200,
+        "payload": {
+            "matriz": createBattleshipGame()
+        }
     }
 
-app.run(debug=True)
+def create_room():
+  room={
+    "jogador_um": None,
+    "jogador_dois": None,
+    "tabuleiro_jogador_um": None,
+    "tabuleiro_jogador_dois": None,
+    "placar": None
+  }
+  return mongo.db.salas.insert_one(room).inserted_id
+
+def update_room(id, data):
+  mongo.db.salas.update_one(
+    {'_id': ObjectId(id)}, {'$set': data}
+  )
+  response = mongo.db.salas.find_one({'_id': ObjectId(id)})
+  
+  return json.loads(json_util.dumps(response))
+
+def getIdOfPlayer(player):
+  if player == None:
+    return None
+  else:
+    try:
+      id = player['$oid']
+      return ObjectId(id)
+    except:
+      return ObjectId(player)
+
+def creatRoomAndInsertPlayer(userId):
+  roomId = create_room()
+  roomData = {
+    "jogador_um": userId,
+    "jogador_dois": None,
+    "tabuleiro_jogador_um": None,
+    "tabuleiro_jogador_dois": None,
+    "placar": None
+  }
+  update_room(roomId, roomData)
+  return getRoomData(roomId)
+  
+def checkIfRoomIsFull(room):
+  return room['jogador_um'] != None and room['jogador_dois'] != None
+
+def getAvaibleRoom(rooms):
+  for room in rooms:
+    if checkIfRoomIsFull(room) == False:
+      return room
+  return None
+
+def insertPlayerInRoom(room, userId):
+  if room['jogador_um'] == None:
+    room['jogador_um'] = ObjectId(userId)
+  else:
+    room['jogador_dois'] = ObjectId(userId)
+  roomData = {
+    "jogador_um": getIdOfPlayer(room['jogador_um']),
+    "jogador_dois": getIdOfPlayer(room['jogador_dois']),
+    "tabuleiro_jogador_um": room['tabuleiro_jogador_um'],
+    "tabuleiro_jogador_dois": room['tabuleiro_jogador_dois'],
+    "placar": room['placar']
+  }
+  update_room(room['_id']['$oid'], roomData)
+  return getRoomData(room['_id']['$oid'])
+        
+def insert_user_in_room(userId):
+  rooms = mongo.db.salas.find()
+  rooms = json_util.dumps(list(rooms), indent=2)
+  # Não existem salas cadastradas
+  if rooms == '[]':
+    return creatRoomAndInsertPlayer(userId)
+  else:
+    roomsJson = json.loads(rooms)
+    # Verifica se existe alguma sala com pelo menos uma vaga
+    selectedRoom = getAvaibleRoom(roomsJson)
+    
+    # Caso não existir, cria uma sala nova e insere o jogador
+    if selectedRoom == None:
+      return creatRoomAndInsertPlayer(userId)
+    # Caso exista uma sala vazia, insere o jogador nela
+    response = insertPlayerInRoom(selectedRoom, userId)
+    if checkIfRoomIsFull(selectedRoom):
+      return initGame(selectedRoom)
+    return(response)
+
+def initGame(room):
+  score = {
+    "jogador_um": {
+      "jogador": getUserData(getIdOfPlayer(room['jogador_um'])),
+      "pontos": 0
+    },
+    "jogador_dois": {
+      "jogador": getUserData(getIdOfPlayer(room['jogador_dois'])),
+      "pontos": 0
+    },
+  }
+
+  battleshipPlayerOne = {
+    "tabuleiro": createBattleshipGame(),
+    "navios_destruidos": []
+  }
+
+  battleshipPlayerTwo = {
+    "tabuleiro": createBattleshipGame(),
+    "navios_destruidos": []
+  }
+
+  roomData = {
+    "jogador_um": getIdOfPlayer(room['jogador_um']),
+    "jogador_dois": getIdOfPlayer(room['jogador_dois']),
+    "tabuleiro_jogador_um": battleshipPlayerOne,
+    "tabuleiro_jogador_dois": battleshipPlayerTwo,
+    "placar": score
+  }
+
+  response = update_room(room['_id']['$oid'], roomData)
+  response['jogador_um'] = getUserData(getIdOfPlayer(room['jogador_um']))
+  response['jogador_dois'] = getUserData(getIdOfPlayer(room['jogador_dois']))
+  return response
+
+def getRoomData(roomId):
+  response = mongo.db.salas.find_one({'_id': ObjectId(roomId)})
+  roomData = json.loads(json_util.dumps(response))
+  roomData['jogador_um'] = getUserData(getIdOfPlayer(roomData['jogador_um']))
+  roomData['jogador_dois'] = getUserData(getIdOfPlayer(roomData['jogador_dois']))
+  return roomData
+
+def getUserData(userId):
+  response = mongo.db.usuarios.find_one({'_id': ObjectId(userId)})
+  return json.loads(json_util.dumps(response))
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    # Receiving Data
+    nome = request.form.get('nome')
+    if nome:
+        id = mongo.db.usuarios.insert_one({'nome': nome}).inserted_id
+        response = {
+          "payload": insert_user_in_room(id),
+          "status_code": 201
+        }
+        return response
+    else:
+        return not_found()
+
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = mongo.db.usuarios.find()
+    response = json_util.dumps(users)
+    return Response(response, mimetype="application/json")
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'message': 'Resource Not Found ' + request.url,
+        'status': 404
+    }
+    response = jsonify(message)
+    response.status_code = 404
+    return response
+
+app.run(debug=True, port=5002)
--- servidorSocket.py
+++ servidorSocket.py
@@ -1,54 +1,190 @@
-from flask import Flask 
-from flask_socketio import SocketIO, send, emit
+import threading
+import time
+
 import requests
-import json
+from flask import Flask, request
+from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-rooms = []
+sala = dict()
+disconectedsSockets = []
+HOST = 'http://127.0.0.1:5002'
 
+# faz uma requisição para a api de gerar um board
+
+
+def gen_board():
+    response = requests.get(f'{HOST}/gerar-campo')
+    return response.json()
+
+#Testado
 def createUser(name):
-    response = requests.post('http://127.0.0.1:5001/users', {"nome": name})
+    response = requests.post(f'{HOST}/users', {"nome": name})
     return response.json()
 
 
-def saveRoom(newRoom):
-    print('\n\nNOVA SALA', newRoom, '\n\n')
-    if(newRoom['pode_iniciar'] == True):
-        print('PASSOU')
-        emit('iniciar_partida', {
-            'mensagem':
-                'A sala '+newRoom['_id']['$oid'] +' iniciou a partida para os jogadores: ' + newRoom['jogador_um']['nome']+ ' e '+ newRoom['jogador_dois']['nome'],
-            'response': {
-                'jogador_um': newRoom['jogador_um'],
-                'jogador_dois': newRoom['jogador_dois'],
-                'sala': newRoom['_id'],
-            }
-        }, broadcast=True)
-    for index, room in enumerate(rooms):
-        if room['_id']['$oid'] == newRoom ['_id']['$oid']:
-            rooms.pop(index)
-            rooms.append(newRoom)
-            return
-    rooms.append(newRoom)
-   
+@socketio.on('chat')
+def chat(data):
+    emit("receiver-msg", data["data"], room=data["room"])
 
 
-@socketio.on('message')
-def handleMessage(msg):
-	print('Message: ' + msg)
-	send(msg, broadcast=True)
+@socketio.on('exit')
+def chat(id):
+    delete_room_or_player(id)
 
-@socketio.on('entrar_sala',)
-def handleMessage(playerName):
-    print('sala criada para '+playerName)
+# gera um board para o usuário
 
-    createUserResponse = createUser(playerName)
-    print("\n\n", type(createUserResponse), "\n\n")
-    saveRoom(createUserResponse['payload'])
-    emit('entrar_sala', {'mensagem':'você está coectado na sala '+playerName, 'response': createUserResponse})
+
+@socketio.on('generate_board')
+def generate_board(data):
+    response = gen_board()
+    board = response["payload"]["matriz"]
+    id = data["id"]
+    room = data["room"]
+    resp = {
+        "board": board,
+        "id": id
+    }
+    emit("resp_gen_board", resp, room=room)
+
+
+# Verifica se a sala está cheia
+def verify_room():
+    for i in sala:
+        if sala[i]["size"] == 1:
+            return i
+
+
+# Salva o board do usuario
+@socketio.on("board")
+def send_board(data):
+    room = data["room"]
+    data_board = get_player(room, data["id"])
+    board = data["board"]
+    data_board["board"] = board
+    emit("send_board", sala[room], room=room)
+    time.sleep(1)
+    emit("get_board", sala[room], room=room)
+
+# altera um board de acordo com as posiçoes passadas
+
+
+@socketio.on("alter_board")
+def send_board(data):
+    id = data["id"]
+    room = data["room"]
+    x = data["x"]
+    y = data["y"]
+    current_player = get_player(room, id)
+    player = get_adversary(room, id)
+    if player["board"][x][y] == '0':
+        player["board"][x][y] = 'Q'
+        current_player["myturn"] = False
+        player["myturn"] = True
+        sala[room]["acertou"] = False
+    else:
+        player["board"][x][y] = 'W'
+        player["placar"] = player["placar"]+1
+        current_player["myturn"] = True
+        player["myturn"] = False
+        sala[room]["acertou"] = True
+        player["acertos"] = player["acertos"] + 1
+        if player["acertos"] == 19:
+            player["ganhou"] = True
+            current_player["ganhou"] = False
+    player["cliques"] = player["cliques"] + 1
+    emit("send_board", sala[room], room=room)
+
+# Verifica se um usuario se desconectou de uma sala
+
+
+@socketio.on("disconnect")
+def disconect():
+    disconectedsSockets.append(request.sid)
+    delete_room_or_player(request.sid)
+    remove()
+
+# verifica se tem usuarios desconectados no array
+# se tiver vai remover todos
+
+
+@socketio.on("connection")
+def connection():
+    print('Connected: '+request.sid)
+    remove()
+
+
+def joinRoom(room, num=1):
+    join_room(room)
+    sala[room]["size"] = num
+    emit("id_room", f"Your room: {room}", room=room)
+    print(room)
+    print(sala[room])
+    if (num == 2):
+        emit("room_message", "Sala cheia", room=room)
+
+# Inicia ou adiciona um novo usuario na sala
+
+
+@socketio.on("join")
+def on_join(data):
+    time.sleep(1)
+    room = verify_room()
+    username = data["username"]
+    if username != None:
+        createUser(username)
+    if room == None:
+        room = data["room"]
+        sala[room] = {"size": 0}
+        sala[room]["jogador1"] = {
+            "name": username, "id": request.sid, "placar": 0, "myturn": True, "cliques": 0, "acertos": 0, "ganhou": -1}
+        joinRoom(room)
+    else:
+        sala[room]["jogador2"] = {"name": username,
+                                  "id": request.sid, "placar": 0, "myturn": False, "cliques": 0, "acertos": 0, "ganhou": -1}
+        
+        joinRoom(room, 2)
+
+# Pega os dados do jogador pelo id dele
+
+
+def get_player(room, id):
+    if sala[room]["jogador1"]["id"] == id:
+        return sala[room]["jogador1"]
+    else:
+        return sala[room]["jogador2"]
+
+# Pega o board do adversario
+
+
+def get_adversary(room, id):
+    if sala[room]["jogador1"]["id"] == id:
+        return sala[room]["jogador2"]
+    else:
+        return sala[room]["jogador1"]
+
+# Deleta um jogador ou uma sala
+
+
+def delete_room_or_player(id):
+    try:
+        for idx, dictionary in enumerate(sala):
+            if sala[dictionary]["jogador1"]["id"] == id or sala[dictionary]["jogador2"]["id"] == id:
+                del sala[dictionary]
+                emit("disconected", "Sala excluida!", room=dictionary)
+    except:
+        print("O tamanho mudou!")
+
+
+def remove():
+    if len(disconectedsSockets) > 0:
+        for i in disconectedsSockets:
+            delete_room_or_player(i)
+            disconectedsSockets.remove(i)
+
 
 if __name__ == '__main__':
-	socketio.run(app)
+    socketio.run(app, debug=True, port=5000)
